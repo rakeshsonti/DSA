92cadaa19bccc951f62c1c36a4c52e92a210704f
Rambhajan Sonti <ramsonti0597@gmail.com>
2022-10-15 15:42:54 +0530

    Update stl.cpp

--- STL/stl.cpp
+++ STL/stl.cpp
@@ -1,4 +1,4 @@
- using namespace std;
+using namespace std;
 namespace ram{
 	int val=45;
 	int getVal(){
@@ -6,12 +6,12 @@ namespace ram{
 	}
 }
 int main(){
-int val=34;
-cout<<ram::val<<endl;	
-cout<<ram::getVal()<<endl;	
+	int val=34;
+	cout<<ram::val<<endl;	
+	cout<<ram::getVal()<<endl;	
 }
 ---------------------------------------------------------
-  Struct data structure
+ # structure
    struct Node{
 	  string name;
 	  int age;
@@ -22,14 +22,28 @@ cout<<ram::getVal()<<endl;
 	  };
   };
 int main(){
-struct Node st;
-//wrong way
-st.name="ram";
-st.age=34;
-cout<<st.name<<","<<st.age<<endl;
-//correct way
-Node* nm=new Node("raja",23);
-cout<<nm->name<<","<<nm->age<<endl;
+	struct Node st;
+	//wrong way
+	st.name="ram";
+	st.age=34;
+	cout<<st.name<<","<<st.age<<endl;
+	//correct way
+	Node* nm=new Node("raja",23);
+	cout<<nm->name<<","<<nm->age<<endl;
 	
 }
 -----------------------------------------
+//Array-> 
+int arr2[10];//{?,?,?}
+array<int,10> arr;//->{?,?,?}
+array<int,10> arr3={}//-> {0,0,0}
+array<int,5> arr1={1,2,3};//{1,2,3,0,0}
+	for(int i=0;i<10;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+----------------------------------------------	
+	
+	
+	
+	
