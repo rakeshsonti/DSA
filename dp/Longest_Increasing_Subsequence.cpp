465ec048d43d37eaa7626f0ee55ffac3d3dbffed
Rakesh Sonti <sontirakes@gmail.com>
2023-01-16 17:47:14 +0530

    Update Longest_Increasing_Subsequence.cpp

--- dp/Longest_Increasing_Subsequence.cpp
+++ dp/Longest_Increasing_Subsequence.cpp
@@ -31,7 +31,38 @@ int longestIncreasingSubsequence(int nums[], int n)
     return maximum;
 }
 */
+//-----------------------------------------------------------------------------------------------------------
+//Algorithmic approach (Binary Search)
+#include<bits/stdc++.h>
+//TC-> O(N*log(n)) SC-> O(N)
 
+int longestIncreasingSubsequence(int nums[], int n)
+{
+/*   Lower bound give curr[i] or first index> curr[i]
+      1. Lower bound give curr[i] or first index> curr[i]
+      2. intution: we will maintain a temp array
+      3. if value is exact match then replace it
+      4. if value is greater than the max value of array then insert at the end
+      5. if value is less than the smallest value of the array then replace 0 index value
+      6. if value dones not exists but occur in between the array value then it will replace the just greater of the search value
+      7. we are finding the length of the longest subsequence but maintaining a temp array
+      8. this temp array is developed by replacing the value
+      9. lower bound work on binary search
+    */
+    vector<int> res;
+    res.push_back(nums[0]);
+    for(int i=1;i<n;i++){
+        if(nums[i]>res.back()){
+            res.push_back(nums[i]);
+        }else{
+            vector<int> ::iterator itr; itr=lower_bound(res.begin(),res.end(),nums[i]);
+            int ind=itr-res.begin();
+            res[ind]=nums[i];
+        }
+    }
+    return res.size();
+}
+//-----------------------------------------------------------------------------------------------------------------
 #include<bits/stdc++.h>
 //TC-> o(N*N) SC-> O(N)
 int longestIncreasingSubsequence(int nums[], int n)
