43074d506267707a923e6ef1a74c8f865096a169
Rakesh Sonti <sontirakes@gmail.com>
2023-01-26 00:10:17 +0530

    Update 904._Fruit_Into_Baskets.cpp

--- sliding-windows/904._Fruit_Into_Baskets.cpp
+++ sliding-windows/904._Fruit_Into_Baskets.cpp
@@ -5,12 +5,16 @@ public:
         unordered_map<int,int> mp(3);
         int n=fruits.size(),maxCount=0,left=0,right=0;
         while(right<n){
+          //if value does not exists then add else increase the count 
           mp[fruits[right]]++;
+          //if window size overflow simply start removing from the start till size become less than 2
           while(mp.size()>2){
+            // remove the element else decrease the frequency
             if(mp[fruits[left]]==1)mp.erase(fruits[left]);
             else mp[fruits[left]]--;
             left++;
           }
+          //calculate the max window size till now
           maxCount=max(maxCount,right-left+1);
           right++;
         }
