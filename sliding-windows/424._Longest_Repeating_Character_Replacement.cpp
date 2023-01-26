720d5165e222ce3b78e6cca55957d5298af8438f
Rakesh Sonti <sontirakes@gmail.com>
2023-01-26 10:06:10 +0530

    Update 424._Longest_Repeating_Character_Replacement.cpp

--- sliding-windows/424._Longest_Repeating_Character_Replacement.cpp
+++ sliding-windows/424._Longest_Repeating_Character_Replacement.cpp
@@ -1,4 +1,25 @@
 class Solution {
+  //tc-> O(n) sc->O(256)=constant
+public:
+    int characterReplacement(string s, int k) {
+        int ans=0,n=s.size(),left=0,right=0,maxCount=0;
+        // map<int,int> mp;
+        vector<int> mp(256,0);
+        while(right<n){
+          mp[s[right]]++;
+          maxCount=max(maxCount,mp[s[right]]);
+          if(right-left+1-maxCount>k){
+            mp[s[left]]--;
+            left++;
+          }
+          ans=max(ans,right-left+1);
+          right++;
+        }
+        return ans;
+    }
+};
+//------------------------------------------------------------------------------------------------------
+class Solution {
   //tc-> O(n) sc->O(n)
 public:
     int characterReplacement(string s, int k) {
