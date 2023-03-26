4cda3892ec9ec3e270087e177a69b3dbd9864a97
Rakesh Sonti <sontirakes@gmail.com>
2023-03-26 11:26:16 +0530

    Time: 4 ms (39.41%), Space: 6.3 MB (62.74%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -1,19 +1,23 @@
-class Solution {
-public:
-    bool checkRecord(string s) {
-        int feq[127];
-        memset(feq,0,sizeof(feq));
-        bool isConsecutive=true;
-        int count=0;
-        for(char ch:s){
-          if(ch=='L')count++;
-          else{
-            if(count>=3)isConsecutive=false;
-            count=0;
-          }
-          feq[ch]++;
+class Solution
+{
+    public:
+        bool checkRecord(string s)
+        {
+            int feq[127];
+            memset(feq, 0, sizeof(feq));
+            bool isConsecutive = true;
+            int count = 0;
+            for (char ch: s)
+            {
+                if (ch == 'L') count++;
+                else
+                {
+                    if (count >= 3) isConsecutive = false;
+                    count = 0;
+                }
+                feq[ch]++;
+            }
+            if (count >= 3) isConsecutive = false;
+            return feq['A'] < 2 && (feq['L'] <= 2 || isConsecutive);
         }
-        if(count>=3)isConsecutive=false;
-        return feq['A']<2 && (feq['L']<=2 || isConsecutive);
-    }
 };
