fb3fce918f4067847c165f193ca29272a667175f
Rakesh Sonti <sontirakes@gmail.com>
2023-03-28 00:36:04 +0530

    Time: 203 ms (10.02%), Space: 58.3 MB (5.57%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+      int n=mat.size(),m=mat[0].size();
+      vector<vector<int>> vis(n,vector<int>(m,1));
+      vector<vector<int>> ans(n,vector<int>(m,0));
+      int level=0;
+      queue<vector<int>>q;
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+          if(mat[i][j]==0){
+            q.push({i,j,level});
+            vis[i][j]=0;
+          }
+        }
+      }
+      int x1[]={-1,0,0,1};
+       int y1[]={0,-1,1,0};
+     while(!q.empty()){
+       vector<int> tmp=q.front();
+       q.pop();
+       int a=tmp[0],b=tmp[1],lvl=tmp[2];
+       ans[a][b]=lvl;
+         
+         for(int i=0;i<4;i++){
+           int x=x1[i]+a,y=y1[i]+b;
+           if(x>=0 && x<n && y>=0 && y<m && vis[x][y]==1){
+             vis[x][y]=0;
+             q.push({x,y,lvl+1});
+           }
+       }
+     }//queue
+      return ans;
+    }
+};
