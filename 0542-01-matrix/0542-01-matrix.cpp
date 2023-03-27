e02b7f9737192563731afeeeb22424ce8d594da1
Rakesh Sonti <sontirakes@gmail.com>
2023-03-28 00:39:12 +0530

    Time: 89 ms (62.03%), Space: 34.5 MB (29.42%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -5,11 +5,11 @@ public:
       vector<vector<int>> vis(n,vector<int>(m,1));
       vector<vector<int>> ans(n,vector<int>(m,0));
       int level=0;
-      queue<vector<int>>q;
+      queue<pair<pair<int,int>,int>>q;
       for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
           if(mat[i][j]==0){
-            q.push({i,j,level});
+            q.push({{i,j},level});
             vis[i][j]=0;
           }
         }
@@ -17,16 +17,16 @@ public:
       int x1[]={-1,0,0,1};
        int y1[]={0,-1,1,0};
      while(!q.empty()){
-       vector<int> tmp=q.front();
+       auto tmp=q.front();
        q.pop();
-       int a=tmp[0],b=tmp[1],lvl=tmp[2];
+       int a=tmp.first.first,b=tmp.first.second,lvl=tmp.second;
        ans[a][b]=lvl;
          
          for(int i=0;i<4;i++){
            int x=x1[i]+a,y=y1[i]+b;
            if(x>=0 && x<n && y>=0 && y<m && vis[x][y]==1){
              vis[x][y]=0;
-             q.push({x,y,lvl+1});
+             q.push({{x,y},lvl+1});
            }
        }
      }//queue
