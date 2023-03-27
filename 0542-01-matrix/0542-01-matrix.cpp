dee7c28e5def17be0cf712b70218e329881aab0d
Rakesh Sonti <sontirakes@gmail.com>
2023-03-28 00:44:54 +0530

    Time: 92 ms (54.30%), Space: 34.5 MB (29.42%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,11 +1,16 @@
 class Solution {
+  //tc-> O(nxm) + O(nxmx4)~O(nxm) || sc-> O(nxm)+O(nxm)+O(nxm)~O(nxm)
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
       int n=mat.size(),m=mat[0].size();
+      //sc-> nxm
       vector<vector<int>> vis(n,vector<int>(m,1));
+      //sc-> nxm
       vector<vector<int>> ans(n,vector<int>(m,0));
       int level=0;
+      //sc-> store all the elements so nxm
       queue<pair<pair<int,int>,int>>q;
+      //nxm
       for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
           if(mat[i][j]==0){
@@ -16,6 +21,7 @@ public:
       }
       int x1[]={-1,0,0,1};
        int y1[]={0,-1,1,0};
+      //excute for all the items nxmx4
      while(!q.empty()){
        auto tmp=q.front();
        q.pop();
