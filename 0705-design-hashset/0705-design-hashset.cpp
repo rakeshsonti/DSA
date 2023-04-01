e01c34e57f96317d75a6f9e4f5bb2fd6c1a513a4
Rakesh Sonti <sontirakes@gmail.com>
2023-04-01 15:50:49 +0530

    Time: 365 ms (20.25%), Space: 40.2 MB (81.24%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,78 +1,73 @@
-class Node{
- public:
-  int key;
-  Node * next=NULL;
-  Node(int key){
-    this->key=key;
-    this->next=NULL;
-  }
-  Node(){};
+class Node
+{
+    public:
+        int key;
+    Node *next = NULL;
+    Node(int key)
+    {
+        this->key = key;
+        this->next = NULL;
+    }
+    Node() {};
 };
-class MyHashSet {
-  Node *head=NULL;
-public:
-    MyHashSet() {
-        // head=new Node();
-    }
-    
-    void add(int key) {
-        if(head==NULL)head=new Node(key);
-      else{
-        Node *ptr=head,* prev=head;
-        while(ptr!=NULL){
-          if(ptr->key==key)return;
-          prev=ptr;
-          ptr=ptr->next;
-        }
-        prev->next=new Node(key);
-      }
-    }
-    
-    void remove(int key) {
-       // print(head);
-      // cout<<"remove called...."<<endl;
-          Node * ptr=head,*prev=NULL;
-          while(ptr!=NULL){
-            // cout<<"val->"<<ptr->key<<endl;
-            if(ptr->key==key){
-              // cout<<"matched.."<<endl;
-              if(ptr==head){
-                // cout<<"head matched.."<<endl;
-                head=head->next;
-                return; 
-              }
-              prev->next=ptr->next;
-              // cout<<prev->next->key<<endl;
-              // print(head);
-              break;
+class MyHashSet
+{
+    Node *head = NULL;
+    public:
+        MyHashSet() {}
+
+    void add(int key)
+    {
+        if (head == NULL) head = new Node(key);
+        else
+        {
+            Node *ptr = head, *prev = head;
+            while (ptr != NULL)
+            {
+                if (ptr->key == key) return;
+                prev = ptr;
+                ptr = ptr->next;
             }
-            prev=ptr;
-            ptr=ptr->next;
-          }
+            prev->next = new Node(key);
+        }
     }
-    
-    bool contains(int key) {
-      Node * ptr=head;
-      while(ptr!=NULL){
-        if(ptr->key==key)return true;
-        ptr=ptr->next;
-      }
-      return false;
+
+    void remove(int key)
+    {
+        Node *ptr = head, *prev = NULL;
+        while (ptr != NULL)
+        {
+            if (ptr->key == key)
+            {
+                if (ptr == head)
+                {
+                    head = head->next;
+                    return;
+                }
+                prev->next = ptr->next;
+                break;
+            }
+            prev = ptr;
+            ptr = ptr->next;
+        }
     }
-  void print(Node * curr){
-    cout<<"print start"<<endl;
-    while(curr!=NULL){
-      cout<<curr->key<<"->";
-      curr=curr->next;
+
+    bool contains(int key)
+    {
+        Node *ptr = head;
+        while (ptr != NULL)
+        {
+            if (ptr->key == key) return true;
+            ptr = ptr->next;
+        }
+        return false;
     }
-    cout<<endl;
-  }
 };
 
 /**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet* obj = new MyHashSet();
- * obj->add(key);
- * obj->remove(key);
- * bool param_3 = obj->contains(key);
+ *Your MyHashSet object will be instantiated and called as such:
+ *MyHashSet* obj = new MyHashSet();
+ *obj->add(key);
+ *obj->remove(key);
+ *bool param_3 = obj->contains(key);
  */
