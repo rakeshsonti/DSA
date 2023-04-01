cc0cfc8921f21843d6841ee452abaaa2b70dd07e
Rakesh Sonti <sontirakes@gmail.com>
2023-04-01 14:16:44 +0530

    Time: 31 ms (86.01%), Space: 71.6 MB (13.95%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,68 +10,47 @@
  */
 class Solution {
 public:
-    // ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    //   ListNode * newHead=new ListNode(0);
-    //   ListNode * ptr=newHead;
-    //   int num=0,carry=0;
-    //     while(l1!=NULL && l2!=NULL){
-    //       num=l1->val+l2->val+carry;
-    //       if(num>9){
-    //         carry=num/10;
-    //         num=num%10;
-    //       }else carry=0;
-    //       ptr->next=new ListNode(num);
-    //       ptr=ptr->next;
-    //       l1=l1->next;
-    //       l2=l2->next;
-    //     }
-    //   while(l1!=NULL){
-    //     num=l1->val+carry;
-    //     if(num>9){
-    //         carry=num/10;
-    //         num=num%10;
-    //       }else carry=0;
-    //       ptr->next=new ListNode(num);
-    //       ptr=ptr->next;
-    //       l1=l1->next;
-    //   }
-    //   while(l2!=NULL){
-    //     num=l2->val+carry;
-    //     if(num>9){
-    //         carry=num/10;
-    //         num=num%10;
-    //       }else carry=0;
-    //     // cout<<"num2:"<<num<<"carry:"<<carry<<endl;
-    //       ptr->next=new ListNode(num);
-    //       ptr=ptr->next;
-    //       l2=l2->next;
-    //   }
-    //   if(carry>0){
-    //     ptr->next=new ListNode(carry);
-    //   }
-    //   newHead=newHead->next;
-    //   return newHead;
-    // }
-      ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy=new ListNode();
-        ListNode* temp=dummy;
-        int carry=0;
-        while(l1!=NULL || l2!=NULL || carry){
-            int sum=0;
-            if(l1!=NULL){
-                sum+=l1->val;
-                l1=l1->next;
-            }
-            if(l2!=NULL){
-                sum+=l2->val;
-                l2=l2->next;
-            }
-            sum+=carry;
-            carry=sum/10;
-            ListNode* newnode=new ListNode(sum%10);
-            temp->next=newnode;
-            temp=temp->next;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+      ListNode * newHead=new ListNode(0);
+      ListNode * ptr=newHead;
+      int num=0,carry=0;
+        while(l1!=NULL && l2!=NULL){
+          num=l1->val+l2->val+carry;
+          if(num>9){
+            carry=num/10;
+            num=num%10;
+          }else carry=0;
+          ptr->next=new ListNode(num);
+          ptr=ptr->next;
+          l1=l1->next;
+          l2=l2->next;
         }
-        return dummy->next;
+      while(l1!=NULL){
+        num=l1->val+carry;
+        if(num>9){
+            carry=num/10;
+            num=num%10;
+          }else carry=0;
+          ptr->next=new ListNode(num);
+          ptr=ptr->next;
+          l1=l1->next;
+      }
+      while(l2!=NULL){
+        num=l2->val+carry;
+        if(num>9){
+            carry=num/10;
+            num=num%10;
+          }else carry=0;
+        // cout<<"num2:"<<num<<"carry:"<<carry<<endl;
+          ptr->next=new ListNode(num);
+          ptr=ptr->next;
+          l2=l2->next;
+      }
+      if(carry>0){
+        ptr->next=new ListNode(carry);
+      }
+      newHead=newHead->next;
+      return newHead;
     }
+
 };
