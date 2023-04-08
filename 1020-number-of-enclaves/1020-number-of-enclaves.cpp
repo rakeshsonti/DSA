47a6699e54ce3fb048ca306f82d37ddbd75c6998
Rakesh Sonti <sontirakes@gmail.com>
2023-04-08 12:16:43 +0530

    Time: 79 ms (59.61%), Space: 32.9 MB (33.14%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,56 @@
+class Solution {
+  public: int numEnclaves(vector < vector < int >> & grid) {
+    int n = grid.size(), m = grid[0].size();
+
+    vector < vector < int >> vis(n, vector < int > (m, 0));
+    for (int i = 0; i < m; i++) {
+      //first row
+      if (grid[0][i] == 1)
+        solve(0, i, grid, vis);
+
+      //last row
+      if (grid[n - 1][i] == 1)
+        solve(n - 1, i, grid, vis);
+    }
+
+    for (int j = 0; j < n; j++) {
+      //first col
+      if (grid[j][0] == 1)
+        solve(j, 0, grid, vis);
+
+      //last row
+      if (grid[j][m - 1] == 1)
+        solve(j, m - 1, grid, vis);
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        if (grid[i][j] == 1 && vis[i][j] == 0) count++;
+
+    return count;
+  }
+  void solve(int row, int col, vector < vector < int >> & grid, vector < vector < int >> & vis) {
+    int n = grid.size();
+    int m = grid[0].size();
+    vis[row][col] = 1;
+    int delrow[] = {
+      -1,
+      0,
+      0,
+      1
+    };
+    int delcol[] = {
+      0,
+      -1,
+      1,
+      0
+    };
+    for (int i = 0; i < 4; i++) {
+      int nrow = row + delrow[i];
+      int ncol = col + delcol[i];
+      if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+        solve(nrow, ncol, grid, vis);
+      }
+    }
+  }
+};
