ac199aa2f328a905f457c7db49f258d587d0d881
Rakesh Sonti <sontirakes@gmail.com>
2023-04-08 10:53:16 +0530

    Time: 11 ms (87.78%), Space: 10.5 MB (34.68%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,4 +1,51 @@
 class Solution {
+public:
+  void solve(vector<vector<char>>& board){
+    int n=board.size(),m=board[0].size();
+    vector<vector<int>> vis(n,vector<int>(m,0));
+    for(int j=0;j<m;j++){
+      //first row
+      if(!vis[0][j] && board[0][j]=='O')
+        dfs(0,j,vis,board);
+      //last row
+       if(!vis[n-1][j] && board[n-1][j]=='O')
+        dfs(n-1,j,vis,board);
+    }
+    
+    for(int  i=0;i<n;i++){
+      //first column
+      if(!vis[i][0] && board[i][0]=='O')
+      dfs(i,0,vis,board);
+      //last column
+      if(!vis[i][m-1] && board[i][m-1]=='O')
+      dfs(i,m-1,vis,board);
+    }
+    
+    for(int i=0;i<n;i++)
+      for(int j=0;j<m;j++)
+        if(vis[i][j]==0 && board[i][j]=='O')board[i][j]='X';
+    
+  }
+  void dfs(int row,int col,vector<vector<int>> &vis,vector<vector<char>>& board) {
+    vis[row][col]=1;
+    
+    int delrow[]={-1,0,0,1};
+    int delcol[]={0,-1,1,0};
+    
+    int n=board.size();
+    int m=board[0].size();
+    
+    for(int i=0;i<4;i++){
+      int nrow=row+delrow[i];
+      int ncol=col+delcol[i];
+      if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && board[nrow][ncol]=='O'){
+        dfs(nrow,ncol,vis,board);
+      }
+    }
+  }
+};
+/*
+class Solution {
   //tc-> O(m)+O(n)+O(m*n) +O(m*n)~O(m*n) sc-> O(m*n) queue
 public:
   void solve(vector<vector<char>>& board) {
@@ -41,7 +88,7 @@ public:
       }
   }
 };
-/*
+
 class Solution {
 public:
   int n,m;
