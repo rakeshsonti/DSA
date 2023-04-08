520e28b844b789493c05f9a0455c35cd25d5310d
Rakesh Sonti <sontirakes@gmail.com>
2023-04-08 13:08:04 +0530

    Time: 83 ms (43.55%), Space: 27.6 MB (80.49%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,4 +1,70 @@
 class Solution {
+  //BFS solution
+  //tc-> O(n)+O(m)+O(n*m*4)+O(m*n)
+  //sc-> O(n*m)
+  public: int numEnclaves(vector < vector < int >> & grid) {
+    int n=grid.size();
+    int m=grid[0].size();
+    vector<vector<int>> vis(n,vector<int>(m,0));
+    
+    int delrow[]={-1,0,0,1};
+    int delcol[]={0,-1,1,0};
+    queue<pair<int,int>> q;
+    
+    for(int i=0;i<m;i++){
+      //first row
+      if(grid[0][i]==1){
+        q.push({0,i});
+        vis[0][i]=1;
+      }
+      //last row
+      if(grid[n-1][i]){
+        q.push({n-1,i});
+        vis[n-1][i]=1;
+      }
+    }
+    
+    for(int j=0;j<n;j++){
+      //first col
+      if(grid[j][0]==1){
+        q.push({j,0});
+        vis[j][0]=1;
+      }
+      // last col
+      if(grid[j][m-1]==1){
+        q.push({j,m-1});
+        vis[j][m-1]=1;
+      }
+    }
+   
+    while(!q.empty()){
+      pair<int,int> node=q.front();
+      q.pop();
+      // vis[node.first][node.second]=1;
+      for(int i=0;i<4;i++){
+        int nrow=node.first+delrow[i];
+        int ncol=node.second+delcol[i];
+        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
+           q.push({nrow,ncol});
+          vis[nrow][ncol]=1;
+        }
+      }
+    }
+    int count=0;
+    for(int i=0;i<n;i++){
+      for(int j=0;j<m;j++){
+        if(grid[i][j]==1 && vis[i][j]==0)count++;    
+      }
+    }
+    return count;
+  }
+};
+
+/*
+class Solution {
+  //DFS solution
+  //tc-> O(n)+O(m)+O(n*m*4)
+  //sc-> O(n*m)
   public: int numEnclaves(vector < vector < int >> & grid) {
     int n = grid.size(), m = grid[0].size();
 
@@ -54,3 +120,4 @@ class Solution {
     }
   }
 };
+*/
