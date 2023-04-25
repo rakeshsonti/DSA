7ff75a91fe8ee2bdce96f29d177de604c57218d0
Rakesh Sonti <sontirakes@gmail.com>
2023-04-25 20:39:47 +0530

    Time: 11 ms (39.95%), Space: 11.6 MB (28.97%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -5,10 +5,13 @@ public:
   }
     string largestNumber(vector<int>& nums) {
       vector<string> ans;
+      //O(n)
       for(auto x: nums)ans.push_back(to_string(x));
+      // O(nlog(n))
       sort(ans.begin(),ans.end(),compareFunc);
       if(ans[0]=="0")return "0";
       string result="";
+      //O(n)
       for(auto x: ans)result+=x;
       return result;
     }
