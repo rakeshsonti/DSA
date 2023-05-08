eae307b28e00c237df09032e50f89622c6cc3f8d
Rakesh Sonti <sontirakes@gmail.com>
2023-05-08 12:56:15 +0530

    Time: 15 ms (77.19%), Space: 9.1 MB (86.76%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.cpp
+++ 0126-word-ladder-ii/0126-word-ladder-ii.cpp
@@ -1,11 +1,14 @@
 class Solution
-{
+{ 
+  //tc-> O(nx26xm) where n is the length of the wordlist and m is the length of the word sc-> O(nxm)
     unordered_map<string, int> map;
     vector<vector < string>> ans;
+   	//to store the begin word
     string b;
     private:
         void dfs(string word, vector<string> &seq)
         {
+           	//if the beginWord matched then reverse it and store to the answer and reverse back to the original sequence to process other sequence
             if (word == b)
             {
                 reverse(seq.begin(), seq.end());
@@ -21,9 +24,11 @@ class Solution
                 for (char ch = 'a'; ch <= 'z'; ch++)
                 {
                     word[i] = ch;
+                  //check if new word have not the same level and should be neighbour level
                     if (map.find(word) != map.end() &&
                         map[word] + 1 == steps)
                     {
+                       	//add to sequence and change back to the original one to process with other sequence
                         seq.push_back(word);
                         dfs(word, seq);
                         seq.pop_back();
@@ -38,8 +43,11 @@ class Solution
             queue<string> q;
             b = beginWord;
             unordered_set<string> st(wordList.begin(), wordList.end());
+           	//start from the first word
             q.push({ beginWord });
+           	//assign as step first
             map[beginWord] = 1;
+           	//once stored remove the word from the original list
             st.erase(beginWord);
             int sz = beginWord.size();
             while (!q.empty())
@@ -47,13 +55,16 @@ class Solution
                 string word = q.front();
                 int steps = map[word];
                 q.pop();
+               	//if match with endword then return no need to continue futher
                 if (word == endWord) break;
                 for (int i = 0; i < sz; i++)
                 {
+                   	//take backup original word to restore later
                     char original = word[i];
                     for (char ch = 'a'; ch <= 'z'; ch++)
                     {
                         word[i] = ch;
+                       	//if word matched then store that word to map and remove from the original list
                         if (st.count(word) > 0)
                         {
                             q.push(word);
@@ -64,6 +75,7 @@ class Solution
                     word[i] = original;
                 }
             }
+           	//if end word find then start from the end and track till the beginWord
             if (map.find(endWord) != map.end())
             {
                 vector<string> seq;
