2c5c3e6520e4f738ade41c87cfd018704bca5736
Rakesh Sonti <sontirakes@gmail.com>
2023-05-14 09:19:09 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
+++ Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
@@ -4,6 +4,37 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
+    private:
+    	bool topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int> vis(V,0);
+	    queue<int> q;
+	    //count degree of the vertices (incoming adges)
+	    for(int i=0;i<V;i++){
+	        for(auto ind: adj[i])vis[ind]++;
+	    }
+	    //push the element whoose degree is 0
+	    for(int i=0;i<V;i++)if(vis[i]==0)q.push(i);
+	    int count=0;
+	    while(!q.empty()){
+	        int val=q.front();
+	        q.pop();
+	        count++;
+	        for(auto it:adj[val]){
+	            //decrease the edges and push once incoming adge is zero
+	            vis[it]--;
+	            if(vis[it]==0)q.push(it);
+	        }
+	    }
+	    return (V!=count);
+	}
+    public:
+     bool isCyclic(int V, vector<int> adj[]) {
+         return topoSort(V,adj);
+     }
+};
+/*
+ class Solution{
     //tc-> O(V+E)
     //SC-> O(2N)+O(N)
     private:
@@ -22,7 +53,10 @@ class Solution {
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
+        //for tracking the visiting node
         vector<int> vis(V,0);
+        //for tracking if visiting node is in same direction of oposite direction
+        //in oposite direction will not be consider as a cyclic graph
         vector<int> dfsVis(V,0);
         for(int i=0;i<V;i++){
             if(!vis[i]){
@@ -32,6 +66,7 @@ class Solution {
         return false;
     }
 };
+*/
 
 //{ Driver Code Starts.
 
