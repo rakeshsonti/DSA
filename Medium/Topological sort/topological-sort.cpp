f47df1a2b1d9b98233bd903718e34f474d85a121
Rakesh Sonti <sontirakes@gmail.com>
2023-05-14 08:40:10 +0530

    Updated Solution - GfG to GitHub

--- Medium/Topological sort/topological-sort.cpp
+++ Medium/Topological sort/topological-sort.cpp
@@ -3,18 +3,52 @@
 using namespace std;
 
 // } Driver Code Ends
+
+
+class Solution
+{
+    // tc->O(U+V) sc-> O(U+V)+O(U+V)=O(U+V)
+    //Kahn's Algorithms
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int> vis(V,0);
+	    queue<int> q;
+	    vector<int> ans;
+	    //count degree of the vertices (incoming adges)
+	    for(int i=0;i<V;i++){
+	        for(auto ind: adj[i])vis[ind]++;
+	    }
+	    //push the element whoose degree is 0
+	    for(int i=0;i<V;i++)if(vis[i]==0)q.push(i);
+	    while(!q.empty()){
+	        int val=q.front();
+	        q.pop();
+	        ans.push_back(val);
+	        for(auto it:adj[val]){
+	            //decrease the edges and push once incoming adge is zero
+	            vis[it]--;
+	            if(vis[it]==0)q.push(it);
+	        }
+	    }
+	    return ans;
+	}
+};
+/*
 class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
+	    //DFS Solution
+	   // tc->(V+U) sc-> O(V+U)+O(U+V)=O(U+V)
+	   //this one is a sinple DFS only result stored in stack data structure
 	    stack<int> st;
 	    vector<bool> vis(V,false);
 	    for(int i=0;i<V;i++){
-	            if(vis[i]==false){
-	                dfs(adj,vis,i,st);
-	            }
+	            if(vis[i]==false) dfs(adj,vis,i,st);
 	    }
 	    vector<int> ans;
 	    while(!st.empty()){
@@ -24,16 +58,14 @@ class Solution
 	    return ans;
 	}
 	void dfs(vector<int> adj[],vector<bool> &vis,int val,stack<int> &st){
-	    
 	    vis[val]=true;
 	    for(auto y:adj[val]){
-	        if(vis[y]==false){
-	            dfs(adj,vis,y,st);
-	        }
+	        if(vis[y]==false) dfs(adj,vis,y,st);
 	    }
 	    st.push(val);
 	}
 };
+*/
 
 //{ Driver Code Starts.
 
