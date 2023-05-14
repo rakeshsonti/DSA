f7f73f26c341c1508104466c932a7baf406fbb5e
Rakesh Sonti <sontirakes@gmail.com>
2023-05-14 16:27:15 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -15,6 +15,8 @@ class Solution {
       vector<int> adjrev[v];
       queue<int> q;
       vector<int> indegree(v,0);
+      //create adjcent node in reverse order and count the degree
+      //as togo logical algo work with incoming adges , so we reverse it to work according to the question to work with outgoing adges
       for(int i=0;i<v;i++){
         for(auto it:adj[i]){
           adjrev[it].push_back(i);
@@ -23,6 +25,7 @@ class Solution {
       }
       vector<int> safenode;
       for(int i=0;i<v;i++){
+          //push all the node whoose indegree is 0 because 0 indegree node don't have outgoing node, it's safe 
         if(indegree[i]==0)q.push(i);
       }
       while(!q.empty()){
@@ -34,6 +37,7 @@ class Solution {
           if(indegree[it]==0)q.push(it);
         }
       }
+      //sort as question needed as in sorted order
       sort(safenode.begin(),safenode.end());
       return safenode;
     }
