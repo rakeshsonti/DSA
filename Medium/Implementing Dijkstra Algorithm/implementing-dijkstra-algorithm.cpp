872e7115819755b478bbede962bd35080ecac57a
Rakesh Sonti <sontirakes@gmail.com>
2023-05-28 11:15:13 +0530

    Updated Solution - GfG to GitHub

--- Medium/Implementing Dijkstra Algorithm/implementing-dijkstra-algorithm.cpp
+++ Medium/Implementing Dijkstra Algorithm/implementing-dijkstra-algorithm.cpp
@@ -3,8 +3,51 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution
 {
+    //Solutio using Set
+    //tc-> O(E+log(V)) E is the total edges and V is the total no of nodes
+	//sc-> O(V)
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+       vector<int> dis(V,1e9);
+        set<pair<int,int>> st;
+       //start from the source
+       //distance is of going to the same element using same element as a source
+       //This algo will work if The Graph doesn't contain any negative weight cycle.
+       // if the graph contains negative weight then it will go into infinite loop and if we go any distance it will consume something , negative weight does not exists
+       st.insert({0,S});
+       dis[S]=0;
+       while(!st.empty()){
+           //distance till now
+           int dist=(*st.begin()).first;
+           int node=(*st.begin()).second;
+            st.erase(st.begin(),++st.begin());
+           for(auto it1:adj[node]){
+               int currWeight=it1[1];
+               int currNode=it1[0];
+               //calculate new distance
+               int currDist=dist+currWeight;
+               //update the distance is found better
+               if(dis[currNode]>currDist){
+                   dis[currNode]=currDist;
+                   st.insert({currDist,currNode});
+               }
+           }
+       }
+       return dis;
+    }
+};
+/*
+class Solution
+{
+    //Solutio using Priority Queue (MIN heap)
+    //tc-> O(E+log(V)) E is the total edges and V is the total no of nodes
+	//sc-> O(V)
 	public:
 	//Function to find the shortest distance of all the vertices
     //from the source vertex S.
@@ -12,16 +55,23 @@ class Solution
     {
        vector<int> dis(V,1e9);
        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+       //start from the source
+       //distance is of going to the same element using same element as a source
+       //This algo will work if The Graph doesn't contain any negative weight cycle.
+       // if the graph contains negative weight then it will go into infinite loop and if we go any distance it will consume something , negative weight does not exists
        pq.push({0,S});
        dis[S]=0;
        while(!pq.empty()){
+           //distance till now
            int dist=pq.top().first;
            int node=pq.top().second;
            pq.pop();
            for(auto it1:adj[node]){
                int currWeight=it1[1];
                int currNode=it1[0];
+               //calculate new distance
                int currDist=dist+currWeight;
+               //update the distance is found better
                if(dis[currNode]>currDist){
                    dis[currNode]=currDist;
                    pq.push({currDist,currNode});
@@ -31,6 +81,7 @@ class Solution
        return dis;
     }
 };
+*/
 
 
 //{ Driver Code Starts.
