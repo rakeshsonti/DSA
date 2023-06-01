7def51d508cb1ca3a4d5d6e02e765c01dde36163
Rakesh Sonti <sontirakes@gmail.com>
2023-06-01 09:59:34 +0530

    Time: 25 ms (66.25%), Space: 13 MB (88.74%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,50 +1,70 @@
-class Solution
-{
-    public:
-        int findCheapestPrice(int n, vector<vector < int>> &flights, int src, int dst, int k)
-        {
-            if (src == dst) return 0;
-            vector<pair<int, int>> adj[n];
-            for (int i = 0; i < flights.size(); i++)
-            {
-                adj[flights[i][0]].push_back({ flights[i][1],
-                    flights[i][2] });
-            }
-            queue<pair<int, pair<int, int>>> pq;
+class Solution {
+	//tc-> O(|V| * |E| * k) sc-> O(|V|)
+public:
+int findCheapestPrice(int n, vector<vector < int>> &flights, int src, int dst, int k) {
+if (src == dst) return 0;
+ // Create an adjacency list representation of the graph.
+        vector<pair<int, int>> adj[n];
+        for (int i = 0; i < flights.size(); i++) {
+            adj[flights[i][0]].push_back({ flights[i][1],
+                flights[i][2] });
+        }
 
-            vector<int> dist(n, 1e9);
-            pq.push({ 0,
-                {
-                    src,
-                    0
-                } });
-            dist[src] = 0;
-            while (!pq.empty())
-            {
-                auto node = pq.front();
-                pq.pop();
-                int stop = node.first;
-                int val = node.second.first;
-                int d = node.second.second;
-                for (auto it: adj[val])
-                {
-                    if (stop > k) break;
-                    int newWeight = d + it.second;
-                    int newNode = it.first;
+        // Create a priority queue to store the distances to each vertex.
+        queue<pair<int, pair<int, int>>> pq;
 
-                    if (dist[newNode] > newWeight && stop <= k)
-                    {
-                        pq.push({ stop + 1,
-                            {
-                                newNode,
-                                newWeight
-                            } });
-                        dist[newNode] = newWeight;
-                    }
+        // Initialize the distances to all vertices to infinity.
+        vector<int> dist(n, 1e9);
+
+        // Mark the source vertex as visited.
+        dist[src] = 0;
+
+        // Add the source vertex to the priority queue.
+        pq.push({ 0,
+            {
+                src,
+                0
+            } });
+
+        // While the priority queue is not empty, do the following:
+        while (!pq.empty()) {
+            // Get the vertex with the minimum distance from the priority queue.
+            auto node = pq.front();
+            pq.pop();
+
+            // Get the current stop, the current vertex, and the current distance.
+            int stop = node.first;
+            int val = node.second.first;
+            int d = node.second.second;
+
+            // For each neighbor of the current vertex, do the following:
+            for (auto it: adj[val]) {
+                // If the current stop is greater than `k`, then break.
+                if (stop > k) break;
+
+                // Calculate the new distance to the neighbor.
+                int newWeight = d + it.second;
+                int newNode = it.first;
+
+                // If the new distance is less than the current distance, then do the following:
+                if (dist[newNode] > newWeight && stop <= k) {
+                    // Update the distance to the neighbor.
+                    dist[newNode] = newWeight;
+
+                    // Add the neighbor to the priority queue.
+                    pq.push({ stop + 1,
+                        {
+                            newNode,
+                            newWeight
+                        } });
                 }
             }
-            if (dist[dst] == 1e9) return -1;
-
-            return dist[dst];
         }
+
+        // If the destination vertex is not reachable, then return -1.
+        if (dist[dst] == 1e9) return -1;
+
+        // Return the distance to the destination vertex.
+        return dist[dst];
+    }
 };
