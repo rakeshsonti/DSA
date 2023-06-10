4313c4ad7827df15af3f383f4d71723bcfef194b
Rakesh Sonti <sontirakes@gmail.com>
2023-06-10 11:14:31 +0530

    Time: 132 ms (67.01%), Space: 43.9 MB (20.98%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -14,6 +14,53 @@ class Solution
             dis[k] = 0;
             dis[0] = 0;
            	//min heap
+            set<pair<int,int>>st;
+            st.insert({ 0,
+                k });
+            while (!st.empty())
+            {
+                auto node = *st.begin();
+                st.erase(node);
+                int d = node.first;
+                int nd = node.second;
+                for (auto it1: adj[nd])
+                {
+                    int currDis = d + it1.second;
+                    int currNode = it1.first;
+                    if (currDis < dis[currNode])
+                    {
+                        dis[currNode] = currDis;
+                        st.insert({ currDis,
+                            currNode });
+                    }
+                }
+            }
+            int ans = 0;
+            for (int i = 0; i < dis.size(); i++)
+            {
+                if (dis[i] == 1e9) return -1;
+                if (ans < dis[i]) ans = dis[i];
+            }
+            return ans;
+        }
+};
+/*
+class Solution
+{
+    public:
+        int networkDelayTime(vector<vector < int>> &times, int n, int k)
+        {
+           	//adjacency node 
+            vector<pair<int, int>> adj[n + 1];
+            for (auto it: times)
+            {
+                adj[it[0]].push_back({ it[1],
+                    it[2] });
+            }
+            vector<int> dis(n + 1, 1e9);
+            dis[k] = 0;
+            dis[0] = 0;
+           	//min heap
             priority_queue<pair<int, int>, vector< pair<int, int>>, greater<pair<int, int>>> pq;
             pq.push({ 0,
                 k });
@@ -44,3 +91,4 @@ class Solution
             return ans;
         }
 };
+*/
